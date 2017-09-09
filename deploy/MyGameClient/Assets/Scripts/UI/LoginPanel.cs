5a42854d73778ad28c5cf5c80792e7b452ec5991
hhwhb20 <531681924@qq.com>
2017-09-09 13:37:19 +0800

    zhuanyi

---
+++ deploy/MyGameClient/Assets/Scripts/UI/LoginPanel.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Common;
+using UnityEngine.SceneManagement;
+public class LoginPanel : BasePanel {
+
+    public InputField usernameIF;
+    public InputField passwordIF;
+    public Text hintMessage;
+
+    private LoginRequest loginRequest;
+
+    void Start()
+    {
+        loginRequest = GetComponent<LoginRequest>();
+    }
+
+    public void OnLoginButton()
+    {
+        hintMessage.text = "";
+        loginRequest.Username = usernameIF.text;
+        loginRequest.Password = passwordIF.text;
+        loginRequest.DefaultRequest();
+    }
+    public void OnRegisterButton()
+    {
+        UIManager.Instance.ShowPanel(UIPanelType.Register);
+    }
+    public void OnLoginResponse(ReturnCode returnCode)
+    {
+        if (returnCode == ReturnCode.Success)
+        {
+            // 跳转到下一个场景 
+            //SceneManager.LoadScene("Game");
+            Debug.Log("登录游戏");
+        }
+        else
+        {
+            hintMessage.text = "用户名或密码错误";
+        }
+    }
+
+    public override void OnEnter()
+    {
+        this.gameObject.SetActive(true);
+    }
+
+    public override void OnPause()
+    {
+        
+    }
+
+    public override void OnResume()
+    {
+        
+    }
+
+    public override void OnExit()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
---
+++ deploy/MyGameClient/Assets/UIFramework/Manager/GameRoot.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameRoot : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("start");
+        UIManager.Instance.PushPanel(UIPanelType.Login);
+	}
+	
+
+}
